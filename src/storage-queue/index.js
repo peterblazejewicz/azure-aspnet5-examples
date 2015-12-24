57103d1f7e4eee6f4405de13c9650663395b0007
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-25 00:05:36 +0100

    Add couple of simple queue access methods

--- src/storage-queue/index.js
+++ src/storage-queue/index.js
@@ -6,6 +6,43 @@ nconf.env()
     file: "config.json",
     search: true
   });
+var QueueName = "my-queue";
 var storageName = nconf.get("StorageName");
 var storageKey = nconf.get("StorageKey");
 var dev = nconf.get("NODE_ENV");
+//
+var retryOperations = new azure.ExponentialRetryPolicyFilter();
+var queueSvc = azure.createQueueService(storageName,
+  storageKey).withFilter(retryOperations);
+if(queueSvc) {
+  queueSvc.createQueueIfNotExists(QueueName, function(error, results, response) {
+    if(error) {
+      return;
+    }
+    var created = results;
+    if(created) {
+      console.log("created new queue");
+    } else {
+      console.log("queue already exists");
+    }
+    var ticket = {
+      EventId:  4711,
+      Email: "peter@example.com",
+      NumberOfTickets: 2,
+      OrderDate: Date.UTC
+    };
+    var msg = JSON.stringify(ticket);
+    queueSvc.createMessage(QueueName, msg, function(error, result, response) {
+      if(error) {
+        return;
+      }
+      queueSvc.peekMessages(QueueName, {
+        numOfMessages: 32
+      }, function(error, result, response){
+        if(!error){
+          // Message text is in messages[0].messagetext
+        }
+      });
+    });
+  });
+}
