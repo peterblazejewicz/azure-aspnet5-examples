c4736b5ea5b279e4fd4d833bec8e685949cdc173
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-24 13:28:45 +0100

    Add launch config for DEV

--- src/storage-queue/index.js
+++ src/storage-queue/index.js
@@ -1,1 +1,11 @@
 var azure = require("azure-storage");
+//
+var nconf = require("nconf");
+nconf.env()
+  .file({
+    file: "config.json",
+    search: true
+  });
+var storageName = nconf.get("StorageName");
+var storageKey = nconf.get("StorageKey");
+var dev = nconf.get("NODE_ENV");
