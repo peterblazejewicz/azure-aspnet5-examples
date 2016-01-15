6ff0bf6cb492cd557cbe3643790df048bde861eb
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-01-15 22:40:18 +0100

    Add support for configurations
    - env variables
    - configuration json file
    - user secrets
    - configuration bindings

--- src/QueueGettingStarted/Program.cs
+++ src/QueueGettingStarted/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.Extensions.Configuration;
 
 namespace QueueGettingStarted
 {
@@ -8,6 +9,14 @@ namespace QueueGettingStarted
     {
         public static void Main(string[] args)
         {
+            // configuration
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("./appsettings.json")
+                .AddUserSecrets()
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            // options
+            ConfigurationBinder.Bind(Configuration.GetSection("Azure:Storage"), Options);
             Console.WriteLine("Queue encryption sample");
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
             CloudQueueClient client = storageAccount.CreateCloudQueueClient();
@@ -18,5 +27,12 @@ namespace QueueGettingStarted
             }
             Console.ReadKey();
         }
+        
+        static IConfiguration Configuration { get; set; }
+        static AzureStorageOptions Options {get; set; } = new AzureStorageOptions();
+    }
+    
+    class AzureStorageOptions {
+        public string ConnectionString { get; set; }
     }
 }
